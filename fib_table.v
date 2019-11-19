992f30d0785fd8d81e4d3f50199ca510a6910257
Jake Maschoff <maschoffjake@gmail.com>
2019-11-19 16:39:32 -0700

    starting fib.

--- fib_table.v
+++ fib_table.v
@@ -1,10 +1,24 @@
 /*
-    This module is used for creating the FIB table of the NDN router. The FIB table consists of 
+    This module is used for creating the FIB table of the NDN router
     
 */
 
 module (
-    
+
+    // PIT INPUTS
+    input [63:0] pit_in_prefix,
+    input [4:0] pit_in_len,
+    input fib_out_bit,
+    input start_send_to_pit,
+
+    // DATA INPUTS
+    input [4:0] data_in_len,
+    input [63:0] data_in_prefix,
+    input data_ready,
+
+    // OVERALL INPUTS 
+    input clk
+
 );
 
 endmodule // 
