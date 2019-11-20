22e6bcd6dd41f63a5003873f785767dc2c3ef197
Jake Maschoff <maschoffjake@gmail.com>
2019-11-20 16:15:19 -0700

    finished saving to FIB table.

--- fib_table.v
+++ fib_table.v
@@ -46,12 +46,68 @@ module fib(
 */
 reg hashTable[5:0][9:0][64:0];
 
-// Incoming packet logic
+// INCOMING PACKET LOGIC
+
+/* 
+    Saving logic. Used for saving the incoming prefix data to the fib hash table.
+*/
+parameter wait_state = 0, get_hash = 1,  save_to_fib_table = 2;
+reg [1:0] saving_logic_state;
+reg [1:0] saving_logic_next_state;
+reg [63:0] prefix_saving;
+reg [5:0] len_saving;
+reg [9:0] saving_logic_hash;
+
+always@(data_ready, saving_logic_state) begin
+    case (saving_logic_state)
+        wait_state: begin
+            if (data_ready)
+                saving_logic_next_state <= get_hash;
+            else
+                saving_logic_next_state <= wait_state;
+        end 
+        get_hash: begin
+            hash_prefix_in <= prefix_saving;
+            hash_len_in <= len_saving;
+            saving_logic_next_state = save_to_fib_table;
+        end
+        save_to_fib_table: begin
+            hashTable[len_saving][saving_logic_hash] = {1'b1, prefix_saving};
+            saving_logic_next_state = wait_state;
+        end
+        default:
+            saving_logic_next_state = wait_state;
+    endcase
+end
+
+always@(posedge clk, rst) begin
+    if (rst)
+        saving_logic_state <= wait_state;
+    else
+        saving_logic_state <= saving_logic_next_state;
+
+    // Latch the prefix and len during wait state, so we can save for other states
+    if (saving_logic_state == wait_state) begin
+        prefix_saving <= data_in_prefix;
+        len_saving <= data_in_len;
+    end
+
+    // Latch hash value after sending to values to hash table
+    if (saving_logic_state == get_hash) begin
+        saving_logic_hash <= hash;
+    end
+end
+
+/* 
+    Propogating data. Used for passing prefix to the PIT to ensure that the data was requested. It it was requested,
+    pass the shift register data to the PIT table. Otherwise just drop it.
+*/
 
 
+// OUTGOING PACKET LOGIC
 
-// Outgoing packet logic
-parameter wait_state = 0, get_hash = 1, check_for_valid_prefix = 2;
+// Transmit data from PIT to outgoing data paths after finding longest matching prefix
+parameter check_for_valid_prefix = 2;
 reg [1:0] outgoing_state;
 reg [1:0] outgoing_next_state;
 reg [9:0] hash_value;
@@ -59,7 +115,7 @@ reg [63:0] prefix;
 reg [5:0] len;
 reg [64:0] hashtable_value;
 
-always@(fib_out_bit, rst) begin
+always@(fib_out_bit, rst, outgoing_state) begin
     case (outgoing_state)
         wait_state: begin
             // Wait for flag to send data out!
