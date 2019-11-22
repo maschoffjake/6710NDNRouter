73dbc2dc2d56b718bb3fd83d4b846be4dc498e0b
Jake Maschoff <maschoffjake@gmail.com>
2019-11-22 16:43:07 -0700

    put declaration in wrong spot.

--- fib_table.v
+++ fib_table.v
@@ -84,10 +84,10 @@ always@(data_ready, saving_logic_state) begin
     endcase
 end
 
+integer i;
 always@(posedge clk, posedge rst) begin
     if (rst) begin
         // Reset hash table values to all 0
-        integer i;
         for (i=0; i<64; i=i+1) 
             hashTable[i] <= 10'b0000000000;
         saving_logic_state <= wait_state;
