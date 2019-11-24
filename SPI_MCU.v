c149bee37c8273766eee47a3209daf82d2672375
Jake Maschoff <maschoffjake@gmail.com>
2019-11-24 14:19:10 -0700

    removed non needed files, just doiing 2.

---
+++ SPI_MCU.v
@@ -0,0 +1,15 @@
+/*
+    This SPI module is used to from NDN --> MCU (user).
+    In this set up the NDN module acts as the slave, and the MCU acts as the
+    master.
+*/
+module spi_mcu(
+    input sclk,
+    input mosi,
+    output mosi,
+    input ss
+);
+
+
+
+endmodule 
---
+++ SPI_interface.v
@@ -0,0 +1,5 @@
+/*
+    This SPI module is used to from NDN --> outgoing interface.
+    In this set up the NDN module acts as the master, and the outgoing interface
+    acts as the slave.
+*/
