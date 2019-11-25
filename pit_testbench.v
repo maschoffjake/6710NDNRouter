a34383612fbb2355b2e6d7d2c4d8aaf4b2c3b711
kenta <kenta@lab1-7.eng.utah.edu>
2019-11-25 14:15:18 -0700

    fixed sizes in testbench

--- pit_testbench.v
+++ pit_testbench.v
@@ -1,10 +1,10 @@
 module pit_testbench();
 
-reg [63:0] table_entry_tb;
+reg [11:0] table_entry_tb;
 reg [7:0] in_data_tb, read_data_tb;
 reg in_bit_tb, out_bit_tb, clk_tb, reset_tb;
 
-wire [61:0] address_tb;
+wire [9:0] address_tb;
 wire [7:0] out_data_tb;
 wire start_bit_tb, write_enable_tb, fib_out_tb;
 
@@ -33,7 +33,7 @@ parameter waiting = 3'b010;
 
 initial
 begin
-	table_entry_tb = 64'b1101000100010001000100010001000100010001000100010001000100010001;
+	table_entry_tb = 64'b00100010001;
 	//table_entry_tb = 64'b1001000100010001000100010001000100010001000100010001000100010001;
 	read_data_tb = 8'b11111111;
 	in_data_tb = 8'd0;
