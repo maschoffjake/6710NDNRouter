f89e016f3620db9c5be43c35c874344a9fa52092
Jake Maschoff <maschoffjake@gmail.com>
2019-11-25 13:09:28 -0700

    almost done with outgoing. need to grab data correctly.

--- fib_table.v
+++ fib_table.v
@@ -8,7 +8,7 @@ module fib_table(
 
     // PIT INPUTS
     input [63:0] pit_in_prefix,
-    input [5:0] pit_in_len,
+    input [7:0] pit_in_metadata,
     input fib_out_bit,
     input start_send_to_pit,
     input rejected,
@@ -28,9 +28,8 @@ module fib_table(
     output reg [7:0] out_data,
 
     // DATA OUTPUTS
-    output reg [63:0] longest_matching_prefix,
-    output reg [5:0] longest_matching_prefix_len,
-    output reg ready_for_data
+    output reg FIB_to_SPI_data_flag,
+    output reg [7:0] data_FIB_to_SPI
 );
 
 /*
@@ -119,93 +118,162 @@ always@(posedge clk, posedge rst) begin
 
 end
 
-// OUTGOING PACKET LOGIC
+// OUTGOING PACKET LOGIC (only deals with interest packets being sent out from the user, since data packets are 
+// handled by incoming state machine)
 reg [63:0] hash_prefix_out;
 reg [9:0] saved_hash_out;
 wire [9:0] hash_value_out;
 hash HASH_OUTGOING(hash_prefix_out, hash_value_out, clk, rst);
 
-// Transmit data from PIT to outgoing data paths after finding longest matching prefix
-parameter check_for_valid_prefix = 2'd2;
-reg [1:0] outgoing_state;
-reg [1:0] outgoing_next_state;
+// Transmit longest matching prefix, total prefix, and meta data once longest prefix is found
+parameter get_hash = 1, check_for_valid_prefix = 2, send_meta_data_to_spi = 3, send_total_prefix_to_spi = 4, send_longest_prefix_to_spi = 5;
+reg [2:0] outgoing_state;
+reg [2:0] outgoing_next_state;
+
+// Save metadata and prefix once fib_out goes high
 reg [63:0] prefix;
-reg [5:0] len;
-reg [5:0] next_len;
+reg [5:0] length_of_prefix;
+reg [7:0] metadata;
 reg hashtable_value;
 
-// Used for sending out interest packets and data packets
-always@(fib_out_bit, start_send_to_pit, outgoing_state) begin
-    // Ensure no latches
-    longest_matching_prefix <= 0;
-    longest_matching_prefix_len <= 0;
-    hash_prefix_out <= 0;
+// Save the total content request for interest packet
+reg [63:0] total_prefix;
+
+// Counts for what byte has been sent out
+reg [2:0] longest_matching_prefix_count;
+reg [2:0] total_prefix_count;
+
+always@(fib_out_bit, outgoing_state) begin
+
+    // Default values for no latch
     hashtable_value <= 0;
-    next_len <= 0;
+    outgoing_next_state <= 0;
+    hash_prefix_out <= 0;
+
 
     case (outgoing_state)
         wait_state: begin
-            // Wait for flag to send data out!
-            if (fib_out_bit)
-                outgoing_next_state <= get_hash;
-            else
-                outgoing_next_state <= wait_state;          
-        end
-        get_hash: begin
-            // Set hash input values
-            hash_prefix_out <= prefix;
-            outgoing_next_state <= check_for_valid_prefix;
-        end
-        check_for_valid_prefix: begin
-            hashtable_value <= hashTable[len][saved_hash_out];
-            if (hashtable_value) begin
-                // Valid entry, forward to output and then enter wait state for another outgoing packet
-                longest_matching_prefix <= prefix;
-                longest_matching_prefix_len <= len;
-                outgoing_next_state <= wait_state;
+            // If fib out is high but not start to send, we know we that we have data from the user
+            if (fib_out_bit && !start_send_to_pit) begin
+                outgoing_next_state <=  check_for_valid_prefix;
             end
             else begin
-                // Not a valid entry, decrement the length and get a new hash
-                next_len <= len - 1;
-                outgoing_next_state <= get_hash;
-
-                /* 
-                    If no prefix has been found after parsing through the entire hashtable,
-                    just forward a length of 0 so the interface knows that no prefix was found and
-                    just broadcast/send to root NDN router
-                */
-                if (next_len == 0) begin
-                    longest_matching_prefix <= prefix;
-                    longest_matching_prefix_len <= 0;
-                    outgoing_next_state <= wait_state;
-                end
+                outgoing_next_state <= wait_state;
             end
         end
-        default:
+        get_hash: begin
+            // Set hash input
+            hash_prefix_out <= prefix;
+        end
+        check_for_valid_prefix: begin
+            hashtable_value <= hashTable[length_of_prefix][saved_hash_out];
+            if (hashtable_value) begin
+                // This is the longest matching prefix! Send out to the SPI and send the packet out
+                outgoing_next_state <= send_meta_data_to_spi;
+            end
+            // Or if the length is 0, we know we didn't find a prefix so just send out a blank longest matching prefix
+            else if (length_of_prefix == 0) begin
+                outgoing_next_state <= send_meta_data_to_spi;
+            end 
+            else begin
+                // Decrement the length by 1 and set the current bit to 0 (so hash changes to correct value) THIS IS DONE IN SEQUENTIAL BLOCK
+                // Go back to get hash state since we still need to find the right prefix
+                outgoing_next_state <= get_hash;
+            end
+        end
+        send_meta_data_to_spi: begin
+            outgoing_next_state <= send_total_prefix_to_spi;
+        end
+        send_total_prefix_to_spi: begin
+            if (total_prefix_count == 0) begin
+                // Done sending the total prefix, now send the longest prefix to spi
+                outgoing_next_state <= send_longest_prefix_to_spi;  
+            end
+            else begin
+                outgoing_next_state <= send_total_prefix_to_spi;
+            end
+        end
+        send_longest_prefix_to_spi: begin
+            if (longest_matching_prefix_count == 0) begin
+                // Done sending longest matching prefix, back to idle
+                outgoing_next_state <= idle;
+            end
+            else begin
+                outgoing_next_state <= send_longest_prefix_to_spi;
+            end
+        end
+        default: begin
             outgoing_next_state <= wait_state;
+        end
     endcase
 end
 
-always @(posedge clk, posedge rst) begin
-    // Next state logic
-	if (rst)
-		outgoing_state <= 2'b00;
-    // Latch the prefix and len during wait state, so we can save for other states
-    else if (outgoing_state == wait_state) begin
-        prefix <= pit_in_prefix;
-        len <= pit_in_len;
-        outgoing_state <= outgoing_next_state;
-    end
-    // Latch hash during get_hash state to use during next state
-    else if (outgoing_state == get_hash) begin
-        saved_hash_out <= hash_value_out;
-        outgoing_state <= outgoing_next_state;
-    end
-    else if (outgoing_state == transfer_data) begin
-        len <= next_len - 1;
-    end
-	else
-		outgoing_state <= outgoing_next_state;
 
+always @(posedge clk, posedge rst) begin
+	if (rst) begin
+		outgoing_state <= 0;
+        outgoing_next_state <= 0;
+        prefix <= 0;
+        metadata <= 0;
+        hashtable_value <= 0;
+        saved_hash_out <= 0;
+        total_prefix <= 0;
+        total_prefix_count < 0;
+        longest_matching_prefix_count <= 0;
+    end;
+    else begin
+        case (outgoing_state)
+            wait_state: begin
+                // Grab the data from the PIT
+                prefix <= pit_in_prefix;
+                length_of_prefix <= pit_in_metadata[5:0];
+                outgoing_state <= outgoing_next_state;
+
+                // Save the total prefix so we can include that in the interest packet and also the metadata
+                total_prefix <= pit_in_prefix;
+                metadata <= pit_in_metadata;
+
+                // Intialize the counts for when we send the data to the SPI
+                total_prefix_count <= 7;
+                longest_matching_prefix_count <= 7;
+
+            end
+            get_hash: begin
+                // Save the hash value on clk edge
+                saved_hash_out <= hash_value_out;
+                outgoing_state <= outgoing_next_state;
+            end
+            check_for_valid_prefix: begin
+                // If the correct value was not found...
+                if (outgoing_next_state != send_meta_data_to_spi) begin
+                    // Set the current bit to 0 (to decrement it) and decrement our length
+                    prefix[length_of_prefix] <= 0;
+                    length_of_prefix <= length_of_prefix - 1;
+                end
+                else begin
+                    // Otherwise we have the correct values and we need to send them to spi to send out, let SPI know we are sending data next cycle
+                    FIB_to_SPI_data_flag <= 1;
+                    outgoing_state <= outgoing_next_state;
+                end
+            end
+            send_meta_data_to_spi: begin
+                data_FIB_to_SPI <= metadata;
+                outgoing_state <= outgoing_next_state;
+            end
+            send_total_prefix_to_spi: begin
+                data_FIB_to_SPI <= metadata;
+                outgoing_state <= outgoing_next_state;
+                total_prefix_count <= total_prefix_count - 1;
+            end
+            send_longest_prefix_to_spi: begin
+                data_FIB_to_SPI <= metadata;
+                outgoing_state <= outgoing_next_state;
+                longest_matching_prefix_count <= longest_matching_prefix_count - 1;
+            end
+            default: begin
+                outgoing_state <= outgoing_next_state;
+            end
+    endcase
+    end
 end
 endmodule
