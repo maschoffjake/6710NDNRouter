db104177e54140ab12cc6c31735047338ac43675
kenta <kenta@lab1-7.eng.utah.edu>
2019-11-25 21:47:20 -0700

    Changing hash

--- hash.v
+++ hash.v
@@ -63,12 +63,12 @@ localparam [9:0] hfunction [63:0] = '{
 	10'b0001000010,
 	10'b0010111000,
 	10'b0000001000,
-	10'b1000010001,
-	10'b0001111000,
-	10'b0101101000,
-	10'b0010101001,
-	10'b1110101100,
-	10'b0010000001
+	,
+	,
+	,
+	,
+	,
+	
 };
 parameter reset = 0, hashing = 1;
 reg state;
@@ -82,6 +82,200 @@ always@(state, data) begin
 		end
 		hashing: begin
 			hash = 10'd0;
+			if(data[0]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[1]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[2]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[3]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[4]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[5]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[6]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[7]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[8]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[9]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[10]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[11]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[12]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[13]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[14]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[15]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[16]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[17]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[18]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[19]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[20]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[21]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[22]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[23]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[24]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[25]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[26]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[27]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[28]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[29]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[30]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[31]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[32]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[33]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[34]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[35]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[36]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[37]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[38]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[39]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[40]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[41]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[42]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[43]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[44]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[45]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[46]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[47]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[48]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[49]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[50]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[51]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[52]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[53]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[54]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[55]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[56]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[57]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[58]) begin
+					hash = hash ^ hfunction[ii];
+			end
+			if(data[59]) begin
+					hash = hash ^ 10'b1000010001;
+			end
+			if(data[60]) begin
+					hash = hash ^ 10'b0101101000;
+			end
+			if(data[61]) begin
+					hash = hash ^ 10'b0010101001;
+			end
+			if(data[62]) begin
+					hash = hash ^ 10'b1110101100;
+			end
+			if(data[63]) begin
+					hash = hash ^ 10'b0010000001;
+			end
+
+
 			for(ii=0; ii<64; ii=ii+1) begin
 				if(data[ii]) begin
 					hash = hash ^ hfunction[ii];
