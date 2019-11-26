cb725264bf5895152d3d6607320221fc68dd5d0a
Jake Maschoff <maschoffjake@gmail.com>
2019-11-25 18:17:00 -0700

    sensitivity list was off...

--- fib_table.v
+++ fib_table.v
@@ -75,7 +75,7 @@ reg isInterestPacket;
 reg [2:0] prefix_byte_count;
 reg [4:0] data_byte_count;
 
-always@(RX_valid, incoming_data_state) begin
+always@(RX_valid, incoming_data_state, prefix_byte_count, data_byte_count, rejected, start_send_to_pit) begin
     // Default values (no latches)
     incoming_data_next_state <= 0;
     hash_prefix_in <= 0;
