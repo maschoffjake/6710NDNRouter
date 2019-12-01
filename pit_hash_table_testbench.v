f3d3cbe0ee5b7e71387cf75997582b379565aa3b
kenta <kenta@lab1-7.eng.utah.edu>
2019-12-01 14:16:59 -0700

    Added pins module

--- pit_hash_table_testbench.v
+++ pit_hash_table_testbench.v
@@ -19,64 +19,67 @@ initial begin
 	rst = 1;
 	#50
 	rst = 0;
-	
+	#50
 	// New request
 	SPI_to_PIT_prefix = 64'b0010010011111101101111111000000010100110111011110111110110100111;
 	length = 63;
 	out_bit = 1;
-	#200
 	$display("New request");
 	$display("Table Entry: %b", table_entry);
 	$display("PIT in bit: %b", pit_in_bit);
 	$display("Rejected bit: %b", rejected);
+	#20
+	out_bit = 0;
 	#100
 	// New request
 	SPI_to_PIT_prefix = 64'b0000011011100000111010101011011100000111110000100000011110111101;
 	length = 63;
 	out_bit = 1;
-	#100
 	$display("New outgoing request");
 	$display("Table Entry: %b", table_entry);
 	$display("PIT in bit: %b", pit_in_bit);
 	$display("Rejected bit: %b", rejected);
+	#20
+	out_bit = 0;
 	#100
 	// New request that has been requested before
 	SPI_to_PIT_prefix = 64'b0010010011111101101111111000000010100110111011110111110110100111;
 	length = 63;
 	out_bit = 1;
-	#100
 	$display("New outgoing request that has been requested before");
 	$display("Table Entry: %b", table_entry);
 	$display("PIT in bit: %b", pit_in_bit);
 	$display("Rejected bit: %b", rejected);
-	#100
+	#20
 	out_bit = 0;
+	#100
 	// Data coming in that has been requested
 	FIB_to_PIT_prefix = 64'b0010010011111101101111111000000010100110111011110111110110100111;
 	FIB_to_PIT_metadata = 63;
 	prefix_ready = 1;
-	#100
 	$display("Incoming data that has been requested");
 	$display("Table Entry: %b", table_entry);
 	$display("PIT in bit: %b", pit_in_bit);
 	$display("Rejected bit: %b", rejected);
+	#20
+	prefix_ready = 0;
 	#100
 	// Data coming in that hasn't been requested
 	FIB_to_PIT_prefix = 64'b0011111111001010100111110010111111110101100011001101011001101000;
 	FIB_to_PIT_metadata = 63;
 	prefix_ready = 1;
-	#100
 	$display("Incoming data that hasn't been requested");
 	$display("Table Entry: %b", table_entry);
 	$display("PIT in bit: %b", pit_in_bit);
 	$display("Rejected bit: %b", rejected);
 	$display("Data Packet: %b", interest_packet);
+	#20
+	prefix_ready = 0;
 	#100
 	// Data coming in that has been requested
 	FIB_to_PIT_prefix = 64'b0010010011111101101111111000000010100110111011110111110110100111;
 	FIB_to_PIT_metadata = {2'b01, 6'd63};
 	prefix_ready = 1;
-	#100
 	$display("Incoming interest packet that has been requested");
 	$display("Table Entry: %b", table_entry);
 	$display("PIT in bit: %b", pit_in_bit);
